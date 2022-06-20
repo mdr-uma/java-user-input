98530eb0452654424621175a4b513818a083e239
github username <um_2003_8@hotmail.com>
2022-06-20 16:51:07 -0400

    import Scanner from java.util

--- java-user-input/src/App.java
+++ java-user-input/src/App.java
@@ -1,4 +1,7 @@
 
+import java.util.Scanner;
+//Scanner is a class in java.util package used for obtaining the input of the primitive types like int, double, etc. and strings.
+
 public class App {
 	public static void main(String[] args) {
 
