fff85d9184eed9312d531267f563b43424e0a319
github username <um_2003_8@hotmail.com>
2022-06-20 16:56:10 -0400

    create a text input and display the user input

--- java-user-input/src/App.java
+++ java-user-input/src/App.java
@@ -4,7 +4,13 @@ import java.util.Scanner;
 
 public class App {
 	public static void main(String[] args) {
-
+		Scanner input = new Scanner(System.in);
+		
+		System.out.println("Enter a text line: ");
+		
+		String text = input.nextLine();
+		
+		System.out.println("Your line is: " + text);
 	}
 
 }
